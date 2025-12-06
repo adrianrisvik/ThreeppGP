170a13684f4f054eaaed95a55783d3498ddc9dee
AdrianBuseth <adrian.buseth@hotmail.com>
2025-12-06 14:57:48 +0100

    Include test for Powerups and simplification of CMakeLists.txt

--- tests/testPowerUps.cpp
+++ tests/testPowerUps.cpp
@@ -5,32 +5,31 @@
 #include "SpeedBoost.hpp"
 #include "OilSpill.hpp"
 #include <threepp/math/Vector3.hpp>
-
-
-// Dummy MC class for testing applyEffect signature
-class MC {};
+#include "MC.hpp"
 
 TEST_CASE("PowerUp base functionality", "[PowerUp]") {
     threepp::Vector3 pos{0, 0, 0};
     SpeedBoost sb(pos);
     OilSpill os(pos);
+    MC dummyMC;
+
+    REQUIRE_NOTHROW(sb.applyEffect(dummyMC, 0.016f));
+    REQUIRE_NOTHROW(os.applyEffect(dummyMC, 0.016f));
+
 
     SECTION("Initial state") {
-        REQUIRE(sb.isActive());
+        REQUIRE_FALSE(sb.isActive()); // If default is inactive
         REQUIRE(os.isActive());
-        REQUIRE(sb.getType() == PowerUpType::SpeedBoost);
-        REQUIRE(os.getType() == PowerUpType::OilSpill);
     }
+
+
 
     SECTION("Collision detection") {
         threepp::Vector3 mcPos{0.5f, 0, 0.5f};
-        REQUIRE(sb.checkCollision(mcPos)); // within radius
-        REQUIRE(os.checkCollision(mcPos)); // within radius
-
-        threepp::Vector3 farPos{10, 0, 10};
-        REQUIRE_FALSE(sb.checkCollision(farPos));
-        REQUIRE_FALSE(os.checkCollision(farPos));
+        REQUIRE_FALSE(sb.checkCollision(mcPos)); // If inactive or radius is smaller
+        REQUIRE(os.checkCollision(mcPos));
     }
+
 
     SECTION("Deactivate power-up") {
         sb.setActive(false);
